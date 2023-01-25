2f243eba1a391c3baf24b14755ef001790c9f3ca
Luke Zappia <lazappi@users.noreply.github.com>
2023-01-25 17:22:23 +0100

    Tidy bash script

--- run-analysis.sh
+++ run-analysis.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# ==== 0. GET DATASETS ==== #
+echo "Downloading country codes..."
 ./scripts/00-download-country-codes.py --out-file data/00-raw/country-codes.tsv
+
+# ==== 1. TIDY DATASETS ==== #
+echo "Tidying country codes..."
 ./scripts/01-tidy-country-codes.py --out-file data/01-tidied/country-codes.tsv data/00-raw/country-codes.tsv
+echo "Tidying house prices..."
 ./scripts/01-tidy-house-prices.py --out-file data/01-tidied/house-prices.tsv data/00-raw/house-prices.csv
+echo "Tidying property tax..."
+./scripts/01-tidy-property-tax.py --out-file data/01-tidied/property-tax.tsv data/00-raw/property-tax.csv
